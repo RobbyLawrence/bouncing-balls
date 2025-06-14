b650cb0e87285ac9da32184762f81c3dc4c1f7ba
Robert Lawrence <rwl19291@gmail.com>
2025-06-14 13:18:36 -0400

    finished physics

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use raylib;
 use raylib::ffi::Color;
-use raylib::prelude::*;
+use raylib::prelude::{RaylibDraw, RaylibDrawHandle};
 
 pub struct Ball {
-    x: f32,
-    y: f32,
-    radius: f32,
-    color: Color,
-    v_x: i64,
-    v_y: i64,
+    pub x: f32,
+    pub y: f32,
+    pub radius: f32,
+    pub color: raylib::ffi::Color,
+    pub v_x: f32,
+    pub v_y: f32,
 }
 
 impl Ball {
@@ -24,5 +24,27 @@ impl Ball {
             v_x,
             v_y,
         }
+    }
+
+    pub fn update(&mut self, screen_w: f32, screen_h: f32) {
+        // update position
+        self.x += self.v_x;
+        self.y += self.v_y;
+
+        // horizontal bouncing
+        if self.x - self.radius <= 0.0 || self.x + self.radius >= screen_w {
+            self.v_x = -self.v_x;
+            // we clamp just in case it skipped a little
+            self.x = self.x.clamp(self.radius, screen_w - self.radius);
+        }
+
+        if self.y - self.radius <= 0.0 || self.y + self.radius >= screen_h {
+            self.v_y = -self.v_y;
+            self.y = self.y.clamp(self.radius, screen_h - self.radius);
+        }
+    }
+
+    pub fn draw(&self, d: &mut RaylibDrawHandle) {
+        d.draw_circle(self.x as i32, self.y as i32, self.radius, self.color);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use raylib::prelude::*;
 use std::io;
 use std::io::*;
 use std::thread;
+use std::thread::current;
 
 fn main() {
-    print!("How many balls would you like? ");
+    println!("How many balls would you like? ");
     let mut line = String::new();
     io::stdin()
         .read_line(&mut line)
@@ -16,18 +17,19 @@ fn main() {
         .parse::<i64>()
         .expect("unable to convert to integer");
 
-    let (mut rl, thread) = raylib::init().size(960, 720).title("Hello World").build();
+    let (mut rl, thread) = raylib::init().size(960, 720).title("Balls").build();
     let mut counter = 1;
     let mut balls: Vec<Ball> = Vec::with_capacity(num_balls as usize);
 
     let mut rng = rand::rng();
     let screen_w = 960.;
-    let screen_h = 640.;
+    let screen_h = 720.;
+
     for _ in 0..num_balls {
         let x = rng.random_range((0.0 + 10.)..(screen_w - 10.));
         let y = rng.random_range((0.0 + 10.)..(screen_h - 10.));
-        let v_x: f32 = rng.random_range(-5. ..5.);
-        let v_y: f32 = rng.random_range(-5. ..5.);
+        let v_x: f32 = rng.random_range(-0.5..0.5);
+        let v_y: f32 = rng.random_range(-0.5..0.5);
 
         let color = Color::new(
             rng.random_range(0..=255),
@@ -35,18 +37,16 @@ fn main() {
             rng.random_range(0..=255),
             rng.random_range(0..=255),
         );
-
-        balls.push(Ball::new(x, y, 10, color, v_x, v_y));
+        let mut ball = Ball::new(x, y, 10., color.into(), v_x, v_y);
+        balls.push(ball);
     }
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::WHEAT);
-        d.draw_circle(coordinate, coordinate, 100., Color::BLUEVIOLET);
-
-        if counter % ((1. / velocity) as i64) == 0 {
-            coordinate += 1;
+        for ball in balls.iter_mut() {
+            ball.update(screen_w, screen_h);
+            ball.draw(&mut d);
         }
-        counter += 1;
     }
 }
